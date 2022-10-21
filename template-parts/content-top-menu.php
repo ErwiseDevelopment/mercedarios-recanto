1badf948460ec76c1596375ac2c5f5e4c893d09c
ErwiseDevelopment <erwisedev@gmail.com>
2022-10-21 14:01:51 -0300

    dinamics

--- template-parts/content-top-menu.php
+++ template-parts/content-top-menu.php
@@ -4,37 +4,39 @@
 
         <div class="row">
 
-            <div class="col-12">
+            <?php
+                $link_pattern = get_field( 'link_padrao', 'option' );
+                $menu_post_link = $link_pattern . get_field( 'link_menu_editorias', 'option');
+                $request_posts = wp_remote_get( $menu_post_link );
+                 
+                if(!is_wp_error( $request_posts )) :
+                    $body = wp_remote_retrieve_body( $request_posts );
+                    $data = json_decode( $body );
 
-                <div class="row">
+                    if(!is_wp_error( $data )) :
+                        foreach( $data as $rest_post ) :
+            //var_dump($rest_post);
 
-                    <div class="col-lg-3 d-flex justify-content-center align-items-center my-4 my-lg-0">
-                        <a href="<?php echo get_home_url( null, '/' ) ?>">
-                            <img
-                            class="img-fluid"
-                            src="<?php echo get_template_directory_uri()?>/../wp-bootstrap-starter-child/assets/images/mercedarios-org.png"
-                            alt="Mercedários Org">
-                        </a>
-                    </div>
+            ?>
 
-                    <div class="col-lg-9 mb-2 mb-lg-0">
 
+                            <div class="col-12">
+
+                                <div class="row">
+
+                                    <div class="col-lg-3 d-flex justify-content-center align-items-center my-4 my-lg-0">
+                                        <a href="<?php echo get_home_url( null, '/' ) ?>">
+                                            <img
+                                            class="img-fluid"
+                                            src="<?php echo get_template_directory_uri()?>/../wp-bootstrap-starter-child/assets/images/mercedarios-org.png"
+                                            alt="Mercedários Org">
+                                        </a>
+                                    </div>
+
+                                    <div class="col-lg-9 mb-2 mb-lg-0">
 
                                         <div class="row">
-                                            <?php
-                                                    $link_pattern = get_field( 'link_padrao', 'option' );
-                                                    $menu_post_link = $link_pattern . get_field( 'link_menu_editorias', 'option');
-                                                    $request_posts = wp_remote_get( $menu_post_link );
-                                                    
-                                                    if(!is_wp_error( $request_posts )) :
-                                                        $body = wp_remote_retrieve_body( $request_posts );
-                                                        $data = json_decode( $body );
 
-                                                        if(!is_wp_error( $data )) :
-                                                            foreach( $data as $rest_post ) :
-                                                //var_dump($rest_post);
-
-                                                ?>
                                         
                                                 <div class="col-6 col-lg-3 px-1">
                                                     <a
@@ -118,13 +120,14 @@
                                                 </div>
                                                 
                                             <!-- end loop -->
-                    </div>
-                </div>
-            </div>
-        </div>
-        <?php       endforeach;
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+            <?php       endforeach;
                     endif; 
                 endif; 
             ?>
+        </div>
     </div>
 </section>
