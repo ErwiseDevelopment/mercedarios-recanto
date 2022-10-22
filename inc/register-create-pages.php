2b310f924d046103ea4fdf7fc181cb1e9bc352c8
ErwiseDevelopment <erwisedev@gmail.com>
2022-10-22 12:24:59 -0300

    page noticias

--- inc/register-create-pages.php
+++ inc/register-create-pages.php
@@ -15,18 +15,18 @@ function ed_create_page() {
         ));
     }
 
-    if( function_exists('acf_add_options_page') ) {	
-        acf_add_options_page( 
-            array( 
-                'page_title' => 'Informativos', 
-                'menu_title' => 'Informativos', 
-                'menu_slug'  => 'Informativos', 
-                'capability' => 'edit_posts', 
-                //'position'   => '20,3', 
-                'redirect'   => false, 
-                'icon_url'   => 'dashicons-excerpt-view' 
-        ));
-    }
+    // if( function_exists('acf_add_options_page') ) {	
+    //     acf_add_options_page( 
+    //         array( 
+    //             'page_title' => 'Informativos', 
+    //             'menu_title' => 'Informativos', 
+    //             'menu_slug'  => 'Informativos', 
+    //             'capability' => 'edit_posts', 
+    //             //'position'   => '20,3', 
+    //             'redirect'   => false, 
+    //             'icon_url'   => 'dashicons-excerpt-view' 
+    //     ));
+    // }
 
     if( function_exists('acf_add_options_page') ) {	
         acf_add_options_page( 
